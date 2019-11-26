6ee57259a863eadc84384cac07e2653c2decdc66
Anubis29 <alexandrebrunet29@gmail.com>
2019-11-26 10:29:38 +0100

    Added ItemContainerInterface

--- src/src/game/core/Exit.java
+++ src/src/game/core/Exit.java
@@ -1,7 +1,32 @@
 package src.game.core;
 
+/**
+ * @author alexandre
+ * @version 0.1
+ * 
+ * La classe <b>Exit</b> permet d'indiquer 
+ * 
+ * Par défaut il une Exit peut toujours être traversée
+ */
+
 public class Exit {
-	private boolean isOpened;
+	private final String NAME;
+	private final Place PLACE;
 	
+	public Exit(String name, Place place){
+		this.NAME = name;
+		this.PLACE = place;
+	}
 	
+	boolean canCross() {
+		return true;
+	}
+	
+	String getName() {
+		return this.NAME;
+	}
+	
+	Place getPlace() {
+		return this.PLACE;
+	}
 }
---
+++ src/src/game/core/ItemContainer.java
@@ -0,0 +1,9 @@
+package src.game.core;
+
+public interface ItemContainer {
+	public abstract void addItem(Item item);
+	public abstract void removeItem(Item item);
+	public abstract Item getItem(String name);
+	public abstract Item[] getItems();
+	public abstract boolean find(Item item);
+}
