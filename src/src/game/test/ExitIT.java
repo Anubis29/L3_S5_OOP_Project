af27750f36ccd42cebb608401b2feb4dba00dcfb
Anubis29 <alexandrebrunet29@gmail.com>
2019-11-28 21:39:34 +0100

    Added Exit Junit test with 100% coverage

---
+++ src/src/game/test/ExitIT.java
@@ -0,0 +1,21 @@
+package src.game.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import src.game.core.Exit;
+import src.game.core.Place;
+
+public class ExitIT {
+    @Test
+    public void test() {
+        final String NAME = "Exit";
+        final Place PLACE = new Place("Name", "Description");
+        
+        Exit exit = new Exit(NAME, PLACE);
+        assertTrue(exit.canCross());
+        assertSame(exit.getPlace(), PLACE);
+        assertEquals(exit.getName(), NAME);
+    }
+}
